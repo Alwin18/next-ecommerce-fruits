be3725d94fc9cd4db88e9b2d0e8b438ff3ad84f7
Alwin18 <alwinbezarius@gmail.com>
2026-05-16 22:58:14 +0700

    init folder structure

---
+++ components/home/banners.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function banners() {
+  return (
+    <div>banners</div>
+  )
+}
---
+++ components/layout/sideber.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function sideber() {
+  return (
+    <div>sideber</div>
+  )
+}
